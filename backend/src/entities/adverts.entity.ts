0bc5c79e4e7ac902e1657bdc3a4f66e4ef1869dc
Marcus <marcuspvh@gmail.com>
2023-02-15 13:57:41 -0400

    Feat:Ticket005 table  Adverts

---
+++ backend/src/entities/adverts.entity.ts
@@ -0,0 +1,36 @@
+import { Entity, Column,  PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToMany } from "typeorm";
+import { Exclude } from "class-transformer";
+import { v4 as uuid} from "uuid";
+
+
+@Entity("adverts")
+export class Adverts{
+@PrimaryGeneratedColumn("uuid")
+
+readonly id: string
+
+@Column({length: 200})
+title_adverts: string
+
+@Column({length: 10})
+year_adverts: string
+
+@Column({length: 50})
+type_adverts: string
+
+@Column({length: 20})
+kilometers_adverts: string
+
+@Column({length: 240})
+description_adverts: string
+
+@Column({default: true})
+isAvailable: boolean
+
+@CreateDateColumn()
+createdAt_adverts: Date
+
+@UpdateDateColumn()
+updatedAt_adverts: Date
+
+}
