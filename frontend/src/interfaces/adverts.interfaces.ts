d61a02eb925c596b4491761beef180692e07a44d
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-02-23 22:05:49 -0300

    feat: Adverts - service read api by id

--- frontend/src/interfaces/adverts.interfaces.ts
+++ frontend/src/interfaces/adverts.interfaces.ts
@@ -13,3 +13,12 @@ export interface iAdvert {
   year_adverts: number;
   kilometers_adverts: number;
 }
+
+export interface iAdvertUpdate {
+  title_adverts?: string;
+  year_adverts?: number;
+  type_adverts?: string;
+  type_veicule?: string;
+  kilometers_adverts?: number;
+  description_adverts?: string;
+}
--- frontend/src/service/adverts/api_read_id_advert.ts
+++ frontend/src/service/adverts/api_read_id_advert.ts
@@ -1,3 +1,13 @@
-const first = () => { };
+import api from "../api";
 
-export default first;
+const api_read_id_advert = async (id_advert: string) => {
+  try {
+    const res = await api.get(`/adverts/${id_advert}/`);
+
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default api_read_id_advert;
