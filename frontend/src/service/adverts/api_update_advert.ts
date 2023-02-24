63db03a114dd9660c6f494904aea5fb2e7902319
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-02-23 22:09:39 -0300

    feat: Adverts - service update advert

--- frontend/src/service/adverts/api_update_advert.ts
+++ frontend/src/service/adverts/api_update_advert.ts
@@ -1,3 +1,14 @@
-const first = () => { };
+import { iAdvertUpdate } from "../../interfaces/adverts.interfaces";
+import api from "../api";
 
-export default first;
+const api_update_advert = async (id_advert: string, data: iAdvertUpdate) => {
+    try {
+        const res = await api.patch(`/adverts/${id_advert}/`, data)
+
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+export default api_update_advert;
