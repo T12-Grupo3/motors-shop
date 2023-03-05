849d578bc465e9b10d5dad69a4df9474f59b3101
Marcus <marcuspvh@gmail.com>
2023-03-05 19:22:28 -0400

    Ajuste para o Ticket055

--- backend/src/middleware/isUserMiddleware.ts
+++ backend/src/middleware/isUserMiddleware.ts
@@ -1,20 +1,17 @@
 import { Request, Response, NextFunction } from "express"
 
-const isUserMiddleware = (req: Request, res: Response, next: NextFunction) =>{
-    
-    const idIsUser = req.params.id
-    
-    const isUser = req.user.id
-    
-    if(!req.user.isAdm && isUser !== idIsUser  ){
-        return res.status(401).json({
-            message: 'User not autorized'
-        })
+const isUserMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const idIsUser = req.params.id;
+    const isUser = req.user.id;
+  
+    if (!req.user.isAdm && isUser !== idIsUser) {
+      return res.status(401).json({
+        message: "User not authorized",
+      });
     }
-
-   
-
-    next()
-    }
-    
-    export default isUserMiddleware
+  
+    next(); // chame a função next() para continuar a execução da rota
+  };
+  
+  export default isUserMiddleware;
+  
---
+++ backend/src/services/comments/update_comments.service.ts
@@ -0,0 +1,78 @@
+import AppDataSource from "../../data-source";
+import { IImageAdvertsRequest } from "../../interfaces/image_adverts";
+import { ImageAdverts } from "../../entities/image_adverts.entity";
+import { AppError } from "../../errors/appError";
+import { IComments, ICommentsRequest, ICommentsUpdate } from "../../interfaces/comments";
+import { Comments } from "../../entities/comments.entity";
+import { User } from "../../entities/user.entity";
+
+
+// const updateCommentsService = async (commentsID:string, userID:string) => {
+    
+//     const commentsRepository = AppDataSource.getRepository(Comments)
+//     const userRepository = AppDataSource.getRepository(User)
+
+//     const commentsExists = await commentsRepository.findOneBy({
+//         id: commentsID
+//     })
+    
+//     if(!commentsExists){
+//         throw new AppError("Comment not found", 404)
+//     }
+
+//     const userExists = await userRepository.findOneBy({
+//         id: userID
+//     })
+
+   
+  
+//     if(userExists){
+//         await commentsRepository.update(
+//             commentsID,
+//             {        
+              
+                
+//                 user:userExists,                    
+//             }
+//         )
+//     }    
+
+//     return commentsExists
+// }
+
+// export default updateCommentsService
+
+
+
+
+
+
+const updateCommentsService = async({ comments }: ICommentsUpdate, id: string): Promise<ICommentsUpdate>  => {
+
+    const commentsRepository = AppDataSource.getRepository(Comments)
+
+    const findComments = await commentsRepository.findOneBy({
+        id
+    })
+
+    if(!findComments){
+        throw new AppError('Image not found', 404)    
+    }
+
+    
+
+    const commentsUpdated = {
+        comments: comments || findComments!.comments,
+        };
+    
+      await commentsRepository.update(findComments!.id, commentsUpdated);
+    
+      const comment = await commentsRepository.findOneBy({
+        id,
+      });
+    
+      const { ...rest } = comment!;
+      return rest;
+}
+
+export default updateCommentsService
