e2e7b44c077f7547eef408b3067fb0441074aaec
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-03-06 15:10:29 -0300

    feat: Ticket043 - default values

--- frontend/src/pages/ProfileView/index.tsx
+++ frontend/src/pages/ProfileView/index.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CarouselComponent from "../../components/carousel";
 import Footer from "../../components/Footer";
 import NavBar from "../../components/NavBar";
 import ProductCard from "../../components/ProductCard";
@@ -19,18 +20,20 @@ import {
 const ProfileView = () => {
   const { id } = useParams();
   const { auctions, cars, motorcycles } = useContext(AdvertContext);
-  const {user, changeName, firstName, lastName, api_read_user } = useContext(UserContext)
-  
-  const userId = localStorage.getItem('MOTORSSHOP:USERID')
+  const { user, changeName, firstName, lastName, api_read_user } =
+    useContext(UserContext);
 
-  useEffect(()=>{
-    const getNameUser = async()=>{
+  console.log(auctions, cars, motorcycles);
 
-      const nameUser = await api_read_user(userId!)
-      changeName(nameUser.name)
-    }
-  getNameUser()
-  }, [])
+  const userId = localStorage.getItem("MOTORSSHOP:USERID");
+
+  useEffect(() => {
+    const getNameUser = async () => {
+      const nameUser = await api_read_user(userId!);
+      changeName(nameUser.name);
+    };
+    getNameUser();
+  }, []);
 
   return (
     <>
@@ -39,9 +42,8 @@ const ProfileView = () => {
       <ContainerUserProfile>
         <div className="imgProfile">
           <p>
-            {" "}
             {firstName}
-            {lastName}{" "}
+            {lastName}
           </p>
         </div>
         <div className="divName">
@@ -59,86 +61,30 @@ const ProfileView = () => {
       <StyledContainerProfile>
         <StyledProductsContainer>
           <h5>Leilão</h5>
-          <StyledAuction>
-            {auctions.map(
-              ({
-                cover_image_adverts,
-                description_adverts,
-                price_adverts,
-                title_adverts,
-                year_adverts,
-                kilometers_adverts,
-                id,
-              }) => (
-                <ProductCardAuction
-                  key={id}
-                  cover_image_adverts={cover_image_adverts}
-                  description_adverts={description_adverts}
-                  kilometers_adverts={kilometers_adverts}
-                  price_adverts={price_adverts}
-                  title_adverts={title_adverts}
-                  year_adverts={year_adverts}
-                  id={id}
-                />
-              )
-            )}
-          </StyledAuction>
+
+          <CarouselComponent adverts={auctions} />
+
           <h5>Carros</h5>
           <div id="cars">
-            {cars.map(
-              ({
-                cover_image_adverts,
-                description_adverts,
-                id,
-                kilometers_adverts,
-                title_adverts,
-                price_adverts,
-                year_adverts,
-              }) => (
-                <>
-                  <ProductCard
-                    key={id}
-                    cover_image_adverts={cover_image_adverts}
-                    description_adverts={description_adverts}
-                    kilometers_adverts={kilometers_adverts}
-                    title_adverts={title_adverts}
-                    year_adverts={year_adverts}
-                    price_adverts={price_adverts}
-                  />
-                  {/* <EditAdvertModal id_adverts={id} /> */}
-                  <DeleteAdvertsModal id_adverts={id} />
-                  <Link to={`/product/${id}`}>Ver como</Link>
-                </>
-              )
-            )}
+            {cars.map((advert) => (
+              <div key={advert.id}>
+                <ProductCard advert={advert} />
+                <EditAdvertModal advert={advert} />
+                <DeleteAdvertsModal id_adverts={advert.id} />
+                <Link to={`/product/${advert.id}`}>Ver como</Link>
+              </div>
+            ))}
           </div>
           <h5>Motos</h5>
           <div id="motos">
-            {motorcycles.map(
-              ({
-                cover_image_adverts,
-                description_adverts,
-                id,
-                kilometers_adverts,
-                title_adverts,
-                price_adverts,
-                year_adverts,
-              }) => (
-                <>
-                  <ProductCard
-                    key={id}
-                    cover_image_adverts={cover_image_adverts}
-                    description_adverts={description_adverts}
-                    kilometers_adverts={kilometers_adverts}
-                    title_adverts={title_adverts}
-                    year_adverts={year_adverts}
-                    price_adverts={price_adverts}
-                  />
-                  <EditAdvertModal id_adverts={id} />
-                  <DeleteAdvertsModal id_adverts={id} />
-                </>
-              )
-            )}
+            {motorcycles.map((advert) => (
+              <div key={advert.id}>
+                <ProductCard advert={advert} />
+                <EditAdvertModal advert={advert} />
+                <DeleteAdvertsModal id_adverts={advert.id} />
+                <Link to={`/product/${advert.id}`}>Ver como</Link>
+              </div>
+            ))}
           </div>
         </StyledProductsContainer>
       </StyledContainerProfile>
