c3be2917c77a685fc53c0a668f5d8d3c0ad0f181
Marcus <marcuspvh@gmail.com>
2023-03-07 08:07:07 -0400

    Fectory: Ticket056 - link do botão Ver todos anucios, e ajuste das rotas list image advertcom o Advert

--- backend/src/services/image_adverts/list_image_adverts.service.ts
+++ backend/src/services/image_adverts/list_image_adverts.service.ts
@@ -5,7 +5,13 @@ import { ImageAdverts } from "../../entities/image_adverts.entity";
 const ListImageAdvertsService = async () => {
 
     const imageAdvertsRepository = AppDataSource.getRepository(ImageAdverts);
-    const imageAdverts = await imageAdvertsRepository.find();
+    const imageAdverts = await imageAdvertsRepository.find({
+        relations: {
+            advert: true,
+            
+          },
+        });
+        
 
     return imageAdverts;
 
