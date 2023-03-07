a76a974312639f81bdfd9efa6298d7f6509f7635
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-03-07 14:49:37 -0300

    feat: Ticket057 - Home buttons

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import ButtonComponent from "../../components/Button";
 import CarouselComponent from "../../components/carousel";
 import Footer from "../../components/Footer";
 import NavBar from "../../components/NavBar";
@@ -7,6 +8,7 @@ import { AdvertContext } from "../../Context/AdvertContext";
 import {
   StyledContent,
   StyledContentContainer,
+  StyledHomeButtons,
   StyledProductsContainer,
 } from "./style";
 
@@ -23,10 +25,33 @@ const Home = () => {
             <h2>Velocidade e experiência em um lugar feito para você</h2>
             <p>Um ambiente feito para você explorar o seu melhor</p>
           </div>
-          <div>
-            <button>Carros</button>
-            <button>Motos</button>
-          </div>
+          <StyledHomeButtons>
+            <ButtonComponent
+              type="button"
+              onClick={() => (window.location.href = "#cars")}
+              border="grey-10"
+              background_color="brand-2"
+              color="grey-10"
+              hover_background_color="brand-3"
+              hover_border="brand-3"
+              hover_color="brand-4"
+            >
+              Carros
+            </ButtonComponent>
+
+            <ButtonComponent
+              type="button"
+              onClick={() => (window.location.href = "#motorcycles")}
+              border="grey-10"
+              background_color="brand-2"
+              color="grey-10"
+              hover_background_color="brand-3"
+              hover_border="brand-3"
+              hover_color="brand-4"
+            >
+              Motos
+            </ButtonComponent>
+          </StyledHomeButtons>
         </StyledContentContainer>
       </StyledContent>
 
@@ -34,7 +59,7 @@ const Home = () => {
         <h5>Leilão</h5>
         <CarouselComponent adverts={auctions} />
         <h5>Carros</h5>
-        <div className="containers-vehicules">
+        <div className="containers-vehicules" id="cars">
           {cars.map((advert) => (
             <ProductCard key={advert.id} advert={advert} />
           ))}
@@ -42,7 +67,7 @@ const Home = () => {
 
         <h5>Motos</h5>
 
-        <div className="containers-vehicules">
+        <div className="containers-vehicules" id="motorcycles">
           {motorcycles.map((advert) => (
             <ProductCard key={advert.id} advert={advert} />
           ))}
