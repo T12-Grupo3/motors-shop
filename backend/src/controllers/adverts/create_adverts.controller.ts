f9457bdde4297d28df7e282c9217a83502b60683
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-03-09 11:49:45 -0300

    refactor: Ticket017 - post adverts recieve user id

--- backend/src/controllers/adverts/create_adverts.controller.ts
+++ backend/src/controllers/adverts/create_adverts.controller.ts
@@ -4,22 +4,35 @@ import { IAdvertsRequest } from "../../interfaces/adverts";
 import createAdvertsService from "../../services/adverts/create_adverts.service";
 import { IImageAdvertsCreate } from "../../interfaces/image_adverts";
 
+const createAdvertsController = async (req: Request, res: Response) => {
+  const {
+    galery_image,
+    cover_image_adverts,
+    title_adverts,
+    year_adverts,
+    type_adverts,
+    kilometers_adverts,
+    type_veicule,
+    price_adverts,
+    description_adverts,
+  }: IAdvertsRequest = req.body;
 
-const createAdvertsController = async(req: Request, res: Response) => {
+  const userId = req.user.id;
 
-    const {galery_image, cover_image_adverts, title_adverts, year_adverts, type_adverts, kilometers_adverts, type_veicule, price_adverts,  description_adverts, userId }:IAdvertsRequest = req.body;
+  const newAdverts = await createAdvertsService({
+    galery_image,
+    cover_image_adverts,
+    title_adverts,
+    year_adverts,
+    type_adverts,
+    type_veicule,
+    price_adverts,
+    kilometers_adverts,
+    description_adverts,
+    userId,
+  });
 
-    const newAdverts = await createAdvertsService({ galery_image, cover_image_adverts, title_adverts, year_adverts, type_adverts, type_veicule, price_adverts, kilometers_adverts, description_adverts, userId });
-
-    return res.status(201).json(instanceToPlain(newAdverts));
-
-
-    
-
-
+  return res.status(201).json(instanceToPlain(newAdverts));
 };
 
-export default createAdvertsController
-
-
-
+export default createAdvertsController;
